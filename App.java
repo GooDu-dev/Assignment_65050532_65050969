1384c4c541002da4cb237eb6d40d6e819808fd21
GooDuDev <mokumokutv@gmail.com>
2023-12-07 16:27:22 +0700

    Add drawLine and plot in CustomDrawing.java and initail the some meta func for working

--- App.java
+++ App.java
@@ -1,3 +1,17 @@
-public class App {
-    
+import javax.swing.JFrame;
+
+public class App extends JFrame {
+    static final int WIDTH = 800, HEIGHT = 600;
+
+    public App(String title) {
+        setTitle(title);
+        setSize(WIDTH, HEIGHT);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public void draw() {
+        CustomDrawing cd = new CustomDrawing();
+        add(cd);
+    }
 }
--- CustomDrawing.java
+++ CustomDrawing.java
@@ -1,3 +1,44 @@
-public class CustomDrawing {
-    
+import javax.swing.JLabel;
+import java.awt.Graphics;
+
+public class CustomDrawing extends JLabel {
+    private void drawLine(Graphics g, Dot start, Dot dest, int size) {
+        int m = 2 * (dest.y - start.y);
+        int slope_error = m - (dest.x - start.x);
+        for (int x = start.x, y = start.y; x <= dest.x; x++) {
+            plot(g, x, y, size);
+            slope_error += m;
+            if (slope_error >= 0) {
+                y++;
+                slope_error -= 2 * (dest.x - start.x);
+            }
+        }
+    }
+
+    private void plot(Graphics g, int x, int y, int size) {
+        g.fillRect(x, y, size, size);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        drawLine(g, new Dot(5, 10), new Dot(600, 500), 10);
+        super.paintComponent(g);
+    }
+}
+
+class Dot {
+    int x, y;
+
+    public Dot(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
