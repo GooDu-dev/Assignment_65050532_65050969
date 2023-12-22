e6181c96993097b9476f79040c0340eeaf261345
GooDuDev <mokumokutv@gmail.com>
2023-12-22 23:44:41 +0700

    re-to-main

---
+++ Cloud.java
@@ -0,0 +1,17 @@
+import java.awt.Graphics;
+
+import javax.swing.JLabel;
+
+public class Cloud extends JLabel {
+
+    private void draw(Graphics g) {
+        CustomDrawing cd = new CustomDrawing();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        // TODO Auto-generated method stub
+        super.paintComponent(g);
+    }
+
+}
