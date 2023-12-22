7c6b3bd5f57fb2056f39d56c67e1c322e9f33fee
GooDuDev <mokumokutv@gmail.com>
2023-12-22 23:53:31 +0700

    Resize app

--- App.java
+++ App.java
@@ -1,7 +1,7 @@
 import javax.swing.JFrame;
 
 public class App extends JFrame {
-    static final int WIDTH = 800, HEIGHT = 600;
+    static final int WIDTH = 600, HEIGHT = 600;
 
     public App(String title) {
         setTitle(title);
