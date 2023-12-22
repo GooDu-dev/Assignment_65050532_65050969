42abc0806584a5e9edf8080039a8e27c6292534a
hold my beer <shishimaru4237@gmail.com>
2023-12-22 23:50:44 +0700

    sun latest

--- Sun.java
+++ Sun.java
@@ -1,11 +1,33 @@
+import java.awt.Graphics;
+
 import javax.swing.JLabel;
 
 public class Sun extends JLabel{
  CustomDrawing customDrawing = new CustomDrawing();
+ Dot[][] sun = {
+  new Dot[]{
+   new Dot(50, 200),
+   new Dot(250, 100),
+   new Dot(50, 400),
+  },
+  new Dot[]{
+   new Dot(50, 400),
+   new Dot(350, 200),
+   new Dot(50, 600),
+  },
+ };
  
- void drawSun(Dot[][] sun){
+ @Override
+ protected void paintComponent(Graphics g){
+  drawSun(g);
+  super.paintComponent(g);
+ }
+
+ void drawSun(Graphics g){
   for(int i = 0; i < sun.length; i++){
-   
+   for(int j = 0; j < sun[i].length; j++){
+    customDrawing.drawCurve(g, sun[i]);
+   }
   }
  }
 }
