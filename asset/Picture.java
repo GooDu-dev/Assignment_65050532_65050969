688151d4ca4bf6115c9b3f92d6e150baadef8fe5
hold my beer <shishimaru4237@gmail.com>
2024-01-02 01:07:26 +0700

    conflict

--- asset/Picture.java
+++ asset/Picture.java
@@ -6,23 +6,6 @@ import java.awt.Graphics;
 import javax.swing.JLabel;
 
 import asset.draw.Dot;
-<<<<<<< HEAD
-
-public class Picture extends JLabel{
- Sun sun = new Sun();
- Cloud cloud = new Cloud();
-
- @Override
- public void paintComponent(Graphics g){
-  super.paintComponent(g);
-  sun.drawSun(g, new Dot(10, 10));
-  cloud.draw(g);
- }
-
- private Dot Dot(int i, int j) {
-  return null;
- }
-=======
 
 public class Picture extends JLabel {
     Sun sun = new Sun();
@@ -34,5 +17,4 @@ public class Picture extends JLabel {
         sun.drawSun(g);
         cloud.draw(g);
     }
->>>>>>> cbe1039a7643335da9718c80bebe965655b1e1ce
 }
